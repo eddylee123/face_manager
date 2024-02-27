be491d1cf88794da6b763a17c64fa005996cdeef
lwz <786838216@qq.com>
2024-02-27 08:52:51 +0800

    2

--- application/admin/library/Auth.php
+++ application/admin/library/Auth.php
@@ -89,7 +89,6 @@ class Auth extends \fast\Auth
         $this->logined = false; //重置登录状态
         Session::delete("admin");
         Cookie::delete("keeplogin");
-        Cookie::delete("apache_");
 
         return $admin['username'] == 'hr_admin';
     }
