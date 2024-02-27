04280e3b9edc37730a3a2c7ac7e26f3b7686e0b1
lwz <786838216@qq.com>
2024-02-27 10:36:02 +0800

    test4

--- application/admin/library/Auth.php
+++ application/admin/library/Auth.php
@@ -3,6 +3,7 @@
 namespace app\admin\library;
 
 use app\admin\model\Admin;
+use app\cache\BaseCache;
 use fast\Random;
 use fast\Tree;
 use think\Config;
@@ -89,7 +90,7 @@ class Auth extends \fast\Auth
         $this->logined = false; //重置登录状态
         Session::delete("admin");
         Cookie::delete("keeplogin");
-        Cookie::clear('/');
+        Cookie::delete(BaseCache::kww_token);
 
         return $admin['username'] == 'hr_admin';
     }
