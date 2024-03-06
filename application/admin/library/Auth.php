d9e66e0225c1b7d0b253dd6358d53dccd28caf1e
lwz <786838216@qq.com>
2024-03-06 16:27:13 +0800

    拍照token

--- application/admin/library/Auth.php
+++ application/admin/library/Auth.php
@@ -90,6 +90,7 @@ class Auth extends \fast\Auth
         $this->logined = false; //重置登录状态
         Session::delete("admin");
         Cookie::delete("keeplogin");
+        Cookie::delete(BaseCache::kww_token);
 
         return $admin['username'] == 'hr_admin';
     }
