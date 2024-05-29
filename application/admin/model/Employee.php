d8d40cac01e58fc3a3701aaab70dfc1901bedb82
lwz <786838216@qq.com>
2024-05-29 21:18:01 +0800

    调整员工信息

--- application/admin/model/Employee.php
+++ application/admin/model/Employee.php
@@ -32,9 +32,6 @@ class   Employee extends Model
         'sex_text',
         'emp_source_text',
         'marry_text',
-//        'create_time_text',
-//        'update_time_text',
-//        'delete_time_text'
     ];
     
 
@@ -343,40 +340,9 @@ class   Employee extends Model
         return isset($list[$value]) ? $list[$value] : '';
     }
 
-
-    public function getCreateTimeTextAttr($value, $data)
+    public function getUrgencyTelAttr($value, $data)
     {
-        $value = $value ? $value : (isset($data['create_time']) ? $data['create_time'] : '');
-        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
-    }
-
-
-    public function getUpdateTimeTextAttr($value, $data)
-    {
-        $value = $value ? $value : (isset($data['update_time']) ? $data['update_time'] : '');
-        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
-    }
-
-
-    public function getDeleteTimeTextAttr($value, $data)
-    {
-        $value = $value ? $value : (isset($data['delete_time']) ? $data['delete_time'] : '');
-        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
-    }
-
-    protected function setCreateTimeAttr($value)
-    {
-        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
-    }
-
-    protected function setUpdateTimeAttr($value)
-    {
-        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
-    }
-
-    protected function setDeleteTimeAttr($value)
-    {
-        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
+        return !empty($value) ? trim($value) : $value;
     }
 
     public function getRoleImg($emp_id, $cs_list, $kq_list)
